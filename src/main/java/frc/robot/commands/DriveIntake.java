8a25ae55c5feee6f13608380a8de091d2783db39
Jakefromstate <Jakefromstate@users.noreply.github.com>
2020-01-24 16:08:02 -1000

    Intake Ver. 1

---
+++ src/main/java/frc/robot/commands/DriveIntake.java
@@ -0,0 +1,38 @@
+package frc.robot.commands;
+
+import edu.wpi.first.hal.FRCNetComm.tInstances;
+import edu.wpi.first.hal.FRCNetComm.tResourceType;
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+
+public class DriveIntake extends CommandBase {
+
+    public final Subsystem[] m_Funnel;
+    double power = 0;
+
+    public DriveIntake(Subsystem[] funnel, double power) {
+            m_Funnel = funnel;
+            addRequirements(funnel);
+            this.power = power;
+        }
+
+
+
+
+        @Override
+        public void execute() {
+            RobotContainer.funnel.setIntake(power); 
+
+        }
+    }
---
+++ src/main/java/frc/robot/subsystems/ControlPanel.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.commands.StickDrive;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+    public class ControlPanel extends CommandBase {
+
+       
+
+    }
